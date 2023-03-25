b8d8ada9b24d16d34c1f56a5c1e756f771710675
Kodaac <nogueira.hs@hotmail.com>
2023-03-25 11:18:06 -0300

    Adicionando componente Texto

--- src/telas/Cesta.js
+++ src/telas/Cesta.js
@@ -3,22 +3,23 @@ import { Image, StyleSheet, Dimensions, Text, View } from 'react-native';
 
 import topo from '../../assets/topo.png';
 import logo from '../../assets/logo.png';
+import Texto from '../componentes/Texto';
 
 const width = Dimensions.get('screen').width;
 
 export default function Cesta() {
     return <>
         <Image source={topo}  style={estilos.topo} />
-        <Text style={estilos.titulo}>Detalhes da cesta</Text>
+        <Texto style={estilos.titulo}>Detalhes da cesta</Texto>
 
         <View style={estilos.cesta}>
-            <Text style={estilos.nome}>Cesta de Verduras</Text>
+            <Texto style={estilos.nome}>Cesta de Verduras</Texto>
             <View style={estilos.fazenda}>
                 <Image source={logo} style={estilos.imagemFazenda}/>
-                <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+                <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
             </View>
-            <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
-            <Text style={estilos.preco}>R$40,00</Text>
+            <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
+            <Texto style={estilos.preco}>R$40,00</Texto>
         </View>
     </>
 }
@@ -49,13 +50,12 @@ const estilos = StyleSheet.create({
         fontSize: 26,
         lineHeight: 42,
         color: "#464646",
-        fontFamily: "MontserratBold"
+        fontWeight: "bold"
     },
 
     fazenda: {
         flexDirection: "row",
         paddingVertical: 12,
-        fontFamily: "MontserratRegular"
     },
 
     imagemFazenda: {
